d2aaefe9713f54325ebd0f7a7b41347947a2bc5d
Olfatalatas <olfatharits@gmail.com>
2026-06-30 13:20:58 +0700

    chore: configure Vercel deployment for Laravel application via index.php entry point

--- backend-api/public/index.php
+++ backend-api/public/index.php
@@ -5,6 +5,11 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+$compiledViewPath = '/tmp/storage/framework/views';
+if (!file_exists($compiledViewPath)) {
+    @mkdir($compiledViewPath, 0777, true);
+}
+
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
